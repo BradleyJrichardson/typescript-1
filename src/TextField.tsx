12a95550f40df0798613d6bff6b19a430e8c674f
BradleyJrichardson <brdl.richardson@gmail.com>
2020-01-09 13:19:23 +1100

    .

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -14,9 +14,25 @@ export const TextField: React.FC<{ text: String }> = () => {
 };
 
 // can also be down as below and this seems much better
+
+interface Person {
+  firstName: string;
+  lastName: String;
+}
+
 interface Props {
   text: string;
+  ok: boolean;
+  i: number;
+  fn: () => string;
+  obj: {
+    f1: string;
+    f2: string;
+  };
+  person: Person;
 }
+
+// you can also create another interface to pass into an object
 
 export const TextFieldWithPropInterface: React.FC<Props> = () => {
   return (
